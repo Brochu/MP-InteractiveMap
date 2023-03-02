2f79eb8ddf8e666068cd72bc7029f7a8e9e2b1a8
Brochu <alexbrochu1@gmail.com>
2023-03-02 16:59:55 -0500

    [MISC] Added new format script to keep code files formatted

---
+++ format-code.bat
@@ -0,0 +1,3 @@
+clang-format -i --style="{BasedOnStyle: LLVM, IndentWidth: 4, IndentAccessModifiers: false, AccessModifierOffset: -4}" .\src\*.cpp
+clang-format -i --style="{BasedOnStyle: LLVM, IndentWidth: 4, IndentAccessModifiers: false, AccessModifierOffset: -4}" .\include\*.h
+clang-format -i --style="{BasedOnStyle: LLVM, IndentWidth: 4, IndentAccessModifiers: false, AccessModifierOffset: -4}" .\shaders\*.hlsl
