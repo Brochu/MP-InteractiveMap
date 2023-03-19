ba870ed32c5ceedb462022cb564723f43f6da6f5
Brochu <alexbrochu1@gmail.com>
2023-03-19 13:48:26 -0400

    [WIP][ICONS] Changed overlay shaders to use structs for in/out of stages

--- shaders/overlay.hlsl
+++ shaders/overlay.hlsl
@@ -3,6 +3,8 @@ Texture2D missileT : register(t1);
 
 SamplerState s : register(s0);
 
+// TODO: Need to bind the PerFrame Constant Buffer for mvp matrix
+
 struct IconVert {
     float3 pos[6];
     float2 uvs[6];
@@ -11,14 +13,27 @@ struct IconVert {
 StructuredBuffer<IconVert> vertexBuffer : register(t2);
 Buffer<uint> typeBuffer : register(t3); // Might need a mask for 8bit values
 
-void VSMain(in uint VertID : SV_VertexID, out float4 Pos : SV_Position, out float2 Tex : TexCoord0) {
+struct VSIn {
+    uint VertId : SV_VertexID;
+    uint InstId : SV_InstanceID;
+};
+
+struct PSIn {
+    float4 Pos : SV_Position;
+    float2 Uvs : TEXCOORD0;
+};
+
+PSIn VSMain(VSIn input) {
     // TODO: Implement getting the right vertex data based on InstanceId
     // Get the vertex position and UVs to send to pixel shader
-    Pos = float4(0.0, 0.0, 0.0, 1.0);
-    Tex = float2(0.0, 0.0);
+
+    PSIn output;
+    output.Pos = float4(0.0, 0.0, 0.0, 1.0);
+    output.Uvs = float2(0.0, 0.0);
+    return output;
 }
 
-float4 PSMain(float4 pos : SV_Position, float2 tex : TEXCOORD0) : SV_TARGET {
+float4 PSMain(PSIn input) : SV_TARGET {
     // TODO: Use the item type and UVs to sample the right texture for the current icon
     // Maybe look into some effects later on?
     // Will the transparency work here?
