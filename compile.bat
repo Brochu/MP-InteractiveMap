8116b8d4e568d31ac0af189e52ce51ea755d4c93
Brochu <alexbrochu1@gmail.com>
2024-01-12 23:27:49 -0500

    [SETUP] Added msbuild to path envvar

--- compile.bat
+++ compile.bat
@@ -1,7 +1,1 @@
-@echo off
-
-set "buildfolder=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin"
-set "execpath=%buildfolder%\MSBuild.exe"
-
-echo %execpath%
-"%execpath%" -nologo -m ./build/MP-InteractiveMap.sln /p:Configuration=Debug
+msbuild -nologo -m ./build/MP-InteractiveMap.sln /p:Configuration=Debug
